5ec52fed50148abd45d99ba47b99e6bac76dc3b0
nguyenba16 <congba2004@gmail.com>
2025-03-06 15:09:37 +0700

    bai2_BTHH1_CongNgheJava

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 //### Bài 1 (4 điểm):
-//Chỉ sử dụng bán kính $r$, không được sử dụng bất kỳ hằng
+//Chỉ sử dụng bán kính r$, không được sử dụng bất kỳ hằng
 // số nào khác, hãy xấp xỉ diện tích của hình tròn tâm $O(0 ,0)$ bán kính $r$.
 public class Main {
     public static double calculateAreaOfCircle (int r, int points){
